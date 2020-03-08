94c261e4750deec8e6a9722dcbee5bb7e6ff7c8c
KobeBryant2824 <932758198@qq.com>
2020-03-08 20:32:09 +0800

    add adminstrator return api

--- jcart-administration-back/src/main/java/io/jjk/jcartadministrationback/controller/ReturnController.java
+++ jcart-administration-back/src/main/java/io/jjk/jcartadministrationback/controller/ReturnController.java
@@ -7,18 +7,21 @@ import io.jjk.jcartadministrationback.dto.out.PageOutDTO;
 import io.jjk.jcartadministrationback.dto.out.ReturnListOutDTO;
 import io.jjk.jcartadministrationback.dto.out.ReturnShowOutDTO;
 import io.jjk.jcartadministrationback.po.Return;
+import io.jjk.jcartadministrationback.service.ReturnService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/return")
+@CrossOrigin
 public class ReturnController {
 
     @Autowired
-    private
+    private ReturnService returnService;
 
     @GetMapping("/search")
     public PageOutDTO<ReturnListOutDTO> search(ReturnSearchInDTO returnSearchInDTO,
@@ -48,13 +51,38 @@ public class ReturnController {
     }
 
     @GetMapping("/getById")
-    public ReturnShowOutDTO getById(@RequestParam Integer returnId){
-        return null;
+    public ReturnShowOutDTO getById(@RequestParam Integer returnId) {
+
+        Return aReturn = returnService.getById(returnId);
+        ReturnShowOutDTO returnShowOutDTO = new ReturnShowOutDTO();
+        returnShowOutDTO.setReturnId(aReturn.getReturnId());
+        returnShowOutDTO.setOrderId(aReturn.getOrderId());
+        returnShowOutDTO.setOrderTimestamp(aReturn.getOrderTime().getTime());
+        returnShowOutDTO.setCustomerId(aReturn.getCustomerId());
+        returnShowOutDTO.setCustomerName(aReturn.getCustomerName());
+        returnShowOutDTO.setMobile(aReturn.getMobile());
+        returnShowOutDTO.setEmail(aReturn.getEmail());
+        returnShowOutDTO.setStatus(aReturn.getStatus());
+        returnShowOutDTO.setAction(aReturn.getAction());
+        returnShowOutDTO.setProductCode(aReturn.getProductCode());
+        returnShowOutDTO.setProductName(aReturn.getProductName());
+        returnShowOutDTO.setQuantity(aReturn.getQuantity());
+        returnShowOutDTO.setReason(aReturn.getReason());
+        returnShowOutDTO.setOpened(aReturn.getOpened());
+        returnShowOutDTO.setComment(aReturn.getComment());
+        returnShowOutDTO.setCreateTimestamp(aReturn.getCreateTime().getTime());
+        returnShowOutDTO.setUpdateTimestamp(aReturn.getUpdateTime().getTime());
+
+        return returnShowOutDTO;
     }
 
     @PostMapping("/updateAction")
-    public void updateAction(@RequestBody ReturnUpdateActionInDTO returnUpdateActionInDTO){
-
+    public void updateAction(@RequestBody ReturnUpdateActionInDTO returnUpdateActionInDTO) {
+        Return aReturn = new Return();
+        aReturn.setReturnId(returnUpdateActionInDTO.getReturnId());
+        aReturn.setAction(returnUpdateActionInDTO.getAction());
+        aReturn.setUpdateTime(new Date());
+        returnService.update(aReturn);
     }
 
 }
